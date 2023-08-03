db6edb8d596ad0ee16b20bc2275fa76c11941225
takashi145 <t.shuhei0203@gmail.com>
2023-08-03 14:44:34 +0900

    ローディングのz-indexをz-50に修正

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,6 +1,6 @@
 export default function Loading() {
   return (
-    <div className='z-40 fixed inset-0 bg-black/30 dark:bg-white/20'>
+    <div className='z-50 fixed inset-0 bg-black/30 dark:bg-white/20'>
       <div className="flex justify-center items-center h-full" aria-label="読み込み中">
         <div className="animate-spin h-10 w-10 border-4 border-blue-500 rounded-full border-t-transparent"></div>
       </div>
